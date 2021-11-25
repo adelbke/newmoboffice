af7029310495d42afca6bc6b6d50678b2efebe91
Quickinline <adelbouhraoua23@gmail.com>
2021-11-25 10:01:08 +0100

    fixed some parameters in backend/config/server.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'http://beta.newmoboffice.com/api',
   admin: {
+    url: 'http://beta.newmoboffice.com/dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '20b713105e81fc7b9435241fb4b4abfa'),
     },
