a7a1849d83cb222ad87f545c600edfc6c4c62ace
Quickinline <adelbouhraoua23@gmail.com>
2022-02-18 00:08:37 +0100

    added https and updated utils/medias.js url

--- frontend/utils/medias.js
+++ frontend/utils/medias.js
@@ -2,7 +2,7 @@ export function getStrapiMedia(url) {
   // Check if URL is a local path
   if (url.startsWith('/')) {
     // Prepend Strapi address
-    return `http://beta.newmoboffice.com/api${url}`
+    return `https://newmoboffice.com/api${url}`
   }
   // Otherwise return full URL
   return url
