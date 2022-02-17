a74567b61bfae83b4406f613f80aaf363f2c65b9
Quickinline <adelbouhraoua23@gmail.com>
2022-02-18 00:17:56 +0100

    updated backend server.js url

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://beta.newmoboffice.com/api',
+  url: 'http://newmoboffice.com/api',
   admin: {
-    url: 'http://beta.newmoboffice.com/dashboard',
+    url: 'http://newmoboffice.com/dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '20b713105e81fc7b9435241fb4b4abfa'),
     },
