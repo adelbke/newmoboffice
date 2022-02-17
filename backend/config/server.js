d4d5e137c795ed6164290159ea2fca6c84517c85
Quickinline <adelbouhraoua23@gmail.com>
2022-02-18 00:18:25 +0100

    added https to backend url

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://newmoboffice.com/api',
+  url: 'https://newmoboffice.com/api',
   admin: {
-    url: 'http://newmoboffice.com/dashboard',
+    url: 'https://newmoboffice.com/dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '20b713105e81fc7b9435241fb4b4abfa'),
     },
